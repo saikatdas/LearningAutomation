9c1555c79b91e8f5f6f65f0fd500d69957c42005
Saikat Das <dassaikat93@yahoo.com>
2017-04-16 15:47:52 +0530

    type conversion and if elif case syntax

---
+++ ifelif.py
@@ -0,0 +1,10 @@
+print('Please enter student mark')
+Mark=input()
+studentMark=int(Mark)
+if studentMark <=40:
+    print('Fail')
+elif studentMark <=60:
+        print('B grade')
+else:
+     print('Got above 60')
+     
